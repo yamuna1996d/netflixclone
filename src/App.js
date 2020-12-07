ce3bb389607cd712d0e939f03b45531b197a29a1
yamuna1996d <46672811+yamuna1996d@users.noreply.github.com>
2020-12-08 01:42:35 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-
+import Row from './Row';
+import requests from './requests';
+import Banner from './Banner';
+import Nav from './Nav';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Nav/>
+      <h1 className="nh1">Netflix Clone by Yamuna</h1>
+      <Banner/>
+      <Row title="Netflix Originals" 
+      fetchUrl={requests.fetchNetflixOriginals}
+      isLargeRow={true}
+      ></Row>
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending}></Row>
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated}></Row>
+      <Row title="ActionMovies" fetchUrl={requests.fetchActionMovies}></Row>
+      <Row title="ComedyMovies" fetchUrl={requests.fetchComedyMovies}></Row>
+      <Row title="HorrorMovies" fetchUrl={requests.fetchHorrorMovies}></Row>
+      <Row title="RomanceMovies" fetchUrl={requests.fetchRomanceMovies}></Row>
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries}></Row>
     </div>
   );
 }
---
+++ src/Nav.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Nav.css';
+function Nav() {
+    return (
+        <div className="nav">
+            <img className="nav_logo"
+            src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
+            />
+        </div>
+    )
+}
+
+export default Nav
+//https://upload.wikimedia.org/wikipedia/commons/0/0f
